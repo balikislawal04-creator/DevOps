81cf759b3f673ef15cb48f277a9addb0eaf17c13
Balikis <Balikis.lawal04@gmail.com>
2025-08-28 19:40:21 -0400

    Fix IAM group resource definition

--- terraform/iam-group.tf
+++ terraform/iam-group.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_group" "this" {
   for_each = toset(local.effective_groups)
-  name     = each.value
+
+  name = each.key                # group name comes from locals
+  path = "/"                     # IAM groups require a path, default is "/"
 }
