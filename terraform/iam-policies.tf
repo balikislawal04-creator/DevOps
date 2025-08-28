80d2f50fbf64b9e4e9fa8a5a8bc65f4228baf6c4
Balikis <Balikis.lawal04@gmail.com>
2025-08-28 18:07:16 -0400

    Add managed policy map and custom IAM policies (AthenaRead, EC2StartStop, S3ReadOnly, S3ReadWrite)

--- terraform/iam-policies.tf
+++ terraform/iam-policies.tf
@@ -1,19 +1,19 @@
-# terraform/iam-policies.tf
-
-resource "aws_iam_policy" "CloudWatchRead" {
-  name        = "CloudWatchRead"
-  description = "Read-only access to CloudWatch"
+# Athena read-only (via Glue + Athena APIs as needed)
+resource "aws_iam_policy" "AthenaRead" {
+  name        = "AthenaRead"
+  description = "Read-only access to query/list Athena and results in S3"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
-          "cloudwatch:Get*",
-          "cloudwatch:List*",
-          "logs:Get*",
-          "logs:Describe*",
-          "logs:List*"
+        Effect = "Allow"
+        Action = [
+          "athena:Get*",
+          "athena:List*",
+          "glue:Get*",
+          "glue:List*",
+          "s3:GetObject",
+          "s3:ListBucket"
         ]
         Resource = "*"
       }
@@ -21,16 +21,20 @@ resource "aws_iam_policy" "CloudWatchRead" {
   })
 }
 
-resource "aws_iam_policy" "SupportUser" {
-  name        = "SupportUser"
-  description = "Basic support permissions"
+# Start/Stop EC2 instances + describe
+resource "aws_iam_policy" "EC2StartStop" {
+  name        = "EC2StartStop"
+  description = "Allow start/stop and describe EC2 instances"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
-          "support:*"
+        Effect = "Allow"
+        Action = [
+          "ec2:StartInstances",
+          "ec2:StopInstances",
+          "ec2:DescribeInstances",
+          "ec2:DescribeInstanceStatus"
         ]
         Resource = "*"
       }
@@ -38,35 +42,47 @@ resource "aws_iam_policy" "SupportUser" {
   })
 }
 
-resource "aws_iam_policy" "SSMDescribe" {
-  name        = "SSMDescribe"
-  description = "Describe-only Systems Manager permissions"
+# S3 read only (bucket list + get object)
+resource "aws_iam_policy" "S3ReadOnly" {
+  name        = "S3ReadOnly"
+  description = "Read-only access to S3"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect   = "Allow"
-        Action   = [
-          "ssm:Describe*",
-          "ssm:Get*"
-        ]
+        Action   = ["s3:ListBucket"]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["s3:GetObject"]
         Resource = "*"
       }
     ]
   })
 }
 
-resource "aws_iam_policy" "RDSReadOnly" {
-  name        = "RDSReadOnly"
-  description = "Read-only access to RDS"
+# S3 read/write (list, get, put, delete object)
+resource "aws_iam_policy" "S3ReadWrite" {
+  name        = "S3ReadWrite"
+  description = "Read/Write access to S3 objects"
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect   = "Allow"
         Action   = [
-          "rds:Describe*",
-          "rds:List*"
+          "s3:ListBucket"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
         ]
         Resource = "*"
       }
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -38,14 +38,16 @@ locals {
   # ...your other locals...
 
   # Map of custom IAM policies created in iam-policies.tf
-  custom_policy_arns = {
-    AthenaRead     = aws_iam_policy.AthenaRead.arn
-    EC2StartStop   = aws_iam_policy.EC2StartStop.arn
-    S3ReadOnly     = aws_iam_policy.S3ReadOnly.arn
-    S3ReadWrite    = aws_iam_policy.S3ReadWrite.arn
-    CloudWatchRead = aws_iam_policy.CloudWatchRead.arn
-    SupportUser    = aws_iam_policy.SupportUser.arn
-    SSMDescribe    = aws_iam_policy.SSMDescribe.arn
-    RDSReadOnly    = aws_iam_policy.RDSReadOnly.arn
+locals {
+  managed_policy_arns = {
+    AdministratorAccess       = "arn:aws:iam::aws:policy/AdministratorAccess"
+    ReadOnlyAccess            = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+    SecurityAudit             = "arn:aws:iam::aws:policy/SecurityAudit"
+    AmazonS3ReadOnlyAccess    = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+    AmazonS3FullAccess        = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+    AmazonRDSReadOnlyAccess   = "arn:aws:iam::aws:policy/AmazonRDSReadOnlyAccess"
+    CloudWatchReadOnlyAccess  = "arn:aws:iam::aws:policy/CloudWatchReadOnlyAccess"
+    AmazonAthenaFullAccess    = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
   }
 }
+
