71b4005f2822b0e072c7de1659baa98303d8e7a7
Balikis <Balikis.lawal04@gmail.com>
2025-08-28 17:34:37 -0400

    Define effective_* locals for groups/users/policies

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -13,23 +13,28 @@ locals {
   ]
 
   default_users = [
-    "ade","bola","chika","dami","eni","femi","grace","habib","ife",
-    "jide","kunle","lara","mike","nike","ola","peter","queen","remi",
-    "sade","timi","uche","viktor","wale","yemi","zainab",
-    "daniel","lucky","ravi","chandrima","bisi"
+    "ade", "bola", "chika", "dami", "eni", "femi", "grace", "habib", "ife",
+    "jide", "kunle", "lara", "mike", "nike", "ola", "peter", "queen", "remi",
+    "sade", "timi", "uche", "viktor", "wale", "yemi", "zainab",
+    "daniel", "lucky", "ravi", "chandrima", "bisi"
   ]
 
   default_group_policies = {
-    admins        = ["AdministratorAccess"]
-    devops        = ["ReadOnlyAccess","S3ReadWrite","EC2StartStop","CloudWatchRead"]
-    data-eng      = ["SecurityAudit"]
-    secops        = ["ReadOnlyAccess"]
-    readonly      = ["ReadOnlyAccess"]
-    s3-rw         = ["S3ReadWrite"]
-    ec2-ops       = ["EC2StartStop","SSMDescribe"]
-    rds-ro        = ["RDSReadOnlyAccess"]
+    admins       = ["AdministratorAccess"]
+    devops       = ["ReadOnlyAccess", "S3ReadWrite", "EC2StartStop", "CloudWatchRead"]
+    data-eng     = ["SecurityAudit"]
+    secops       = ["ReadOnlyAccess"]
+    readonly     = ["ReadOnlyAccess"]
+    s3-rw        = ["S3ReadWrite"]
+    ec2-ops      = ["EC2StartStop", "SSMDescribe"]
+    rds-ro       = ["RDSReadOnlyAccess"]
     cloudwatch-ro = ["CloudWatchReadOnlyAccess"]
-    support       = ["SupportUser"]
+    support      = ["SupportUser"]
   }
+
+  # 🔹 Add these computed locals
+  effective_groups         = length(var.groups) > 0 ? var.groups : local.default_groups
+  effective_users          = length(var.users) > 0 ? var.users : local.default_users
+  effective_group_policies = length(var.group_policies) > 0 ? var.group_policies : local.default_group_policies
 }
 
