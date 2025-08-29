7ba18f8b40525c07f61ef8127ee5e8370e4d4e77
Balikis <Balikis.lawal04@gmail.com>
2025-08-28 20:01:05 -0400

    Use name_prefix on custom IAM policies to avoid name collisions

--- terraform/iam-policies.tf
+++ terraform/iam-policies.tf
@@ -1,172 +1,109 @@
-# Athena read-only (Glue + Athena APIs as needed)
+# Athena read-only
 resource "aws_iam_policy" "AthenaRead" {
-  name        = "tf-AthenaRead"
+  name_prefix = "tf-AthenaRead-"
   description = "Read-only access to query/list Athena and results in S3"
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "athena:Get*",
-          "athena:List*",
-          "glue:Get*",
-          "glue:List*",
-          "s3:GetObject",
-          "s3:ListBucket"
-        ]
-        Resource = "*"
-      }
-    ]
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["athena:Get*", "athena:List*", "glue:Get*", "glue:List*", "s3:GetObject*", "s3:ListBucket*"]
+      Resource = "*"
+    }]
   })
 }
 
 # Start/Stop EC2 instances + describe
 resource "aws_iam_policy" "EC2StartStop" {
-  name        = "tf-EC2StartStop"
+  name_prefix = "tf-EC2StartStop-"
   description = "Allow start/stop and describe EC2 instances"
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "ec2:StartInstances",
-          "ec2:StopInstances",
-          "ec2:DescribeInstances",
-          "ec2:DescribeInstanceStatus"
-        ]
-        Resource = "*"
-      }
-    ]
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["ec2:StartInstances", "ec2:StopInstances", "ec2:DescribeInstances", "ec2:DescribeInstanceStatus"]
+      Resource = "*"
+    }]
   })
 }
 
-# S3 read only (bucket list + get object)
+# S3 read-only (bucket list + get object)
 resource "aws_iam_policy" "S3ReadOnly" {
-  name        = "tf-S3ReadOnly"
+  name_prefix = "tf-S3ReadOnly-"
   description = "Read-only access to S3"
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["s3:ListBucket"]
-        Resource = "*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = ["s3:GetObject"]
-        Resource = "*"
-      }
+      { Effect = "Allow", Action = ["s3:ListBucket"],  Resource = "*" },
+      { Effect = "Allow", Action = ["s3:GetObject*"],  Resource = "*" }
     ]
   })
 }
 
 # S3 read/write (list, get, put, delete object)
 resource "aws_iam_policy" "S3ReadWrite" {
-  name        = "tf-S3ReadWrite"
+  name_prefix = "tf-S3ReadWrite-"
   description = "Read/Write access to S3 objects"
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
-      {
-        Effect   = "Allow"
-        Action   = ["s3:ListBucket"]
-        Resource = "*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:DeleteObject"
-        ]
-        Resource = "*"
-      }
+      { Effect = "Allow", Action = ["s3:ListBucket"],               Resource = "*" },
+      { Effect = "Allow", Action = ["s3:GetObject*", "s3:PutObject*", "s3:DeleteObject*"], Resource = "*" }
     ]
   })
 }
 
-# CloudWatch read access
+# CloudWatch read-only
 resource "aws_iam_policy" "CloudWatchRead" {
-  name        = "tf-CloudWatchRead"
-  description = "Read-only access to CloudWatch metrics/logs"
+  name_prefix = "tf-CloudWatchRead-"
+  description = "Read-only access to CloudWatch"
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "cloudwatch:Get*",
-          "cloudwatch:List*",
-          "logs:Describe*",
-          "logs:Get*",
-          "logs:List*"
-        ]
-        Resource = "*"
-      }
-    ]
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["cloudwatch:Get*", "cloudwatch:List*", "logs:Get*", "logs:Describe*", "logs:List*"]
+      Resource = "*"
+    }]
   })
 }
 
-# SupportUser (typical read + support center)
+# Support user (basic support console actions)
 resource "aws_iam_policy" "SupportUser" {
-  name        = "tf-SupportUser"
-  description = "Support user read access and support center view"
+  name_prefix = "tf-SupportUser-"
+  description = "Basic AWS Support center read-only"
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "support:*Describe*",
-          "support:*Get*",
-          "support:*List*",
-          "trustedadvisor:*"
-        ]
-        Resource = "*"
-      }
-    ]
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["support:Describe*", "support:List*"]
+      Resource = "*"
+    }]
   })
 }
 
-# SSM Describe
+# SSM describe
 resource "aws_iam_policy" "SSMDescribe" {
-  name        = "tf-SSMDescribe"
-  description = "Describe Systems Manager managed instances and ops data"
+  name_prefix = "tf-SSMDescribe-"
+  description = "Describe-only SSM permissions"
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "ssm:Describe*",
-          "ssm:Get*",
-          "ssm:List*"
-        ]
-        Resource = "*"
-      }
-    ]
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["ssm:Describe*", "ssm:Get*", "ssm:List*"]
+      Resource = "*"
+    }]
   })
 }
 
-# RDS ReadOnly
+# RDS read-only
 resource "aws_iam_policy" "RDSReadOnly" {
-  name        = "tf-RDSReadOnly"
-  description = "Read-only access to RDS APIs"
+  name_prefix = "tf-RDSReadOnly-"
+  description = "Read-only access to RDS metadata"
   policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = [
-          "rds:Describe*",
-          "rds:ListTagsForResource"
-        ]
-        Resource = "*"
-      }
-    ]
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["rds:Describe*", "rds:List*", "rds:DescribeDBInstances"]
+      Resource = "*"
+    }]
   })
 }
-
